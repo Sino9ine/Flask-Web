5daf9fa52ba8fa564b2ea283a1794ae4c25a1230
Sino9ine <BossKuB87@gmail.com>
2025-11-22 19:21:08 +0700

    commit

---
+++ src/app.py
@@ -0,0 +1,23 @@
+# src/app.py
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# 1. Home Page: Route '/'
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# 2. Git Page: Route '/git'
+@app.route('/git')
+def git_summary():
+    return render_template('git.html')
+
+# 3. Docker Page: Route '/docker'
+@app.route('/docker')
+def docker_summary():
+    return render_template('docker.html')
+
+if __name__ == '__main__':
+    # รันบน host 0.0.0.0 และ port 5000 ตามมาตรฐาน Docker/Flask
+    app.run(host='0.0.0.0', port=5000)
